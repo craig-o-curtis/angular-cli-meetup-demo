76507176f34cc44fb884d0bd020a103815231f53
Craig Curtis <ccurtis@unicon.net>
2017-09-16 11:43:41 -0700

    Step-04-Child-Routes

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,7 +1,17 @@
+import { BlogComponent } from '../blog/blog.component';
+import { HomeComponent } from '../home/home.component';
+import { MainComponent } from './main.component';
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent, children: [
+    { path: '', pathMatch: 'full', redirectTo: 'home' },
+    { path: 'home', component: HomeComponent },
+    { path: 'blog', component: BlogComponent } ] },
+  { path: '**', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
+import { HomeModule } from '../home/home.module';
+import { BlogModule } from '../blog/blog.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    MainRoutingModule
+    MainRoutingModule,
+    HomeModule,
+    BlogModule
   ],
   declarations: [MainComponent],
   exports: [MainComponent]
