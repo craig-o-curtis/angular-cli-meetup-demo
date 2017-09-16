2ada202e2fb35f19dac6642ef614468e051b176d
Craig Curtis <ccurtis@unicon.net>
2017-09-16 10:19:00 -0700

    Step-01-HMR

---
+++ e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('craigc-root h1')).getText();
+  }
+}
